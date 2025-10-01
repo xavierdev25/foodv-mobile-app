bb1e5a1c4c9fcd1ab74ce8935050d1426fc5519c
xavierdev25 <xavierdev25@icloud.com>
2025-10-01 01:08:20 -0500

    Actualiza la configuración de Babel y añade nuevas dependencias: se incluye el plugin 'react-native-reanimated/plugin' en babel.config.js y se añaden 'react-native-worklets' y 'react-native-worklets-core' en package.json y package-lock.json.

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
-  let plugins = [];
+  let plugins = ['react-native-reanimated/plugin'];
 
   return {
     presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
